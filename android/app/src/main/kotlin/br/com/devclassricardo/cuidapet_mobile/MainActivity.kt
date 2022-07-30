e969ac18385cd319224cf254a6502369fe7ce9b5
Ricardo Emerson <ricardo_emerson@yahoo.com.br>
2022-07-30 10:25:03 -0300

    Initial project setup.

---
+++ android/app/src/main/kotlin/br/com/devclassricardo/cuidapet_mobile/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.devclassricardo.cuidapet_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app/app_widget.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => MaterialApp.router(
+        title: 'Cuidapet',
+        theme: ThemeData(primarySwatch: Colors.blue),
+        routeInformationParser: Modular.routeInformationParser,
+        routerDelegate: Modular.routerDelegate,
+      );
+}
