2267dcd928a681506eac176bff6eb183f84e41c5
Ricardo Emerson <ricardo_emerson@yahoo.com.br>
2022-07-30 11:28:33 -0300

    Change responsive control to flutter_screenutil.

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,14 +1,27 @@
+import 'package:asuka/asuka.dart' as asuka;
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => MaterialApp.router(
+  Widget build(BuildContext context) {
+    Modular.setObservers([
+      asuka.asukaHeroController,
+    ]);
+
+    return ScreenUtilInit(
+      designSize: const Size(390, 844),
+      builder: (BuildContext context, Widget? child) => MaterialApp.router(
+        debugShowCheckedModeBanner: false,
+        builder: asuka.builder,
         title: 'Cuidapet',
         theme: ThemeData(primarySwatch: Colors.blue),
         routeInformationParser: Modular.routeInformationParser,
         routerDelegate: Modular.routerDelegate,
-      );
+      ),
+    );
+  }
 }
